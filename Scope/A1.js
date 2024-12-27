f0dcab019065d7aeecb516fb8078ab10449fdc2e
SiddharthKhadanga-wq <siddharthkhadanga29@gmail.com>
2024-12-27 12:05:08 +0530

    scope

---
+++ Scope/A1.js
@@ -0,0 +1,34 @@
+// function one() {
+//     const username = "Siddharth"
+
+//     function two() {
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(website)
+//     two()
+
+
+// }
+// one()
+
+// if (true) {
+//     const username = " Sidhu "
+//     if (username === " Sidhu ") {
+//         const website = "Youtube "
+//         console.log(username + website)
+//     }
+
+//     // console.log(website)
+// }
+// console.log(username)
+
+function addone(num) {
+    return num + 1
+}
+console.log(addone(5))
+
+const addtwo = function(num) {
+    return num + 2
+}
+console.log(addtwo(5))
