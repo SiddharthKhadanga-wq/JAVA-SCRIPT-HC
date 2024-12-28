2459e874a1134bad92ede0480cde1e0213c55da7
SiddharthKhadanga-wq <siddharthkhadanga29@gmail.com>
2024-12-28 09:27:01 +0530

    one in control flow commited

---
+++ Controlflow/one.js
@@ -0,0 +1,23 @@
+// const isloggedin = true
+// if (true) {
+//     console.log("Executed")
+// }
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power : ${power}`)
+// }
+// console.log(`user power: ${power}`)
+
+const userlogin = true
+const usercredit = false
+    // if (usercredit && userlogin) {
+    //     console.log("Allow to buy")
+    // } else {
+    //     console.log("Not allow to buy")
+    // }
+
+if (usercredit || userlogin) {
+    console.log("Allow to buy")
+}
